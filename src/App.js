1616addf65b7b6043e15f64adabb2fe34647f050
Priyansh Ragit <priyanshragit@gmail.com>
2022-07-30 14:23:29 +0530

    added upto article link with articleList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+//pages
+import { BrowserRouter as Router,Route,Routes } from "react-router-dom";
+import Home from "./pages/Home"
+import About from "./pages/About"
+import ArticlesList from "./pages/ArticlesList"
+import Article from "./pages/Article"
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <NavBar />
+      <div className='max-w-screen-md mx-auto pt-20'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />}/>
+          <Route path="/articles-list" element={<ArticlesList />}/>
+          <Route path="/article/:name" element={<Article />}/>
+        </Routes> 
+      </div>
+    </Router>
+    
+  )
 }
 
 export default App;
---
+++ src/pages/article-contents.js
@@ -0,0 +1,19 @@
+const articles =[
+    {
+    name :"chicken-tikka",
+    title : "Chicken Tikka",
+    thumbnail:'../images/chicken.jpg',
+    content :[
+        'order from zomato'
+    ]
+    },
+    {
+        name :"paneer-tikka",
+        title : "Paneer Tikka",
+        thumbnail:'../images/paneer.jpg',
+        content :[
+            'order from swiggy'
+        ]
+    }
+]
+export default articles
